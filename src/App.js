c9b833e1d90d7fb686263f34380cb0613a04de3b
KeyaAkter <sanhatanvin12@gmail.com>
2022-11-29 01:21:27 +0600

    project initialized by designing header & footer element

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import Footer from "./components/Footer";
+import Header from "./components/Header";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper bg-gradient-to-t from-blue-600 to-white min-h-screen text-xl text-gray-900 flex flex-col py-10">
+      <Header />
+      <Footer />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="bg-blue-400 container mx-auto text-center p-10 text-sm text-gray-100">
+      <p>
+        &copy; {new Date().getFullYear()} React Todo App. All rights reserved.
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
